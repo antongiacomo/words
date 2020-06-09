a1ebe935d499738b95e8e4946a5092a4c7eeb2a6
Antongiacomo Polimeno <antongiacomo>
2020-06-09 18:38:04 +0200

    Removed Warnings

--- src/App.js
+++ src/App.js
@@ -4,16 +4,16 @@ import "./tailwind.generated.css";
 //
 function App() {
   const [data, setData] = useState([]);
-  const [title, setTitle] = useState('')
-  const config = {
-    headers: { "Access-Control-Allow-Origin": "*" },
-  };
+  const [title, setTitle] = useState("");
+
 
   useEffect(() => {
     const fetchData = async () => {
       const result = await axios(
         "https://cors-anywhere.herokuapp.com/https://work.unimi.it/foProssimiEsami/json/F94",
-        config
+        {
+          headers: { "Access-Control-Allow-Origin": "*" },
+        }
       );
 
       setData(result.data);
@@ -25,22 +25,31 @@ function App() {
   return (
     <div className="">
       <div className="w-full h-10 px-4 my-6">
-      <input className="w-full h-full border py-2 px-4" onChange={event => setTitle(event.target.value)} />
+        <input
+          className="w-full h-full border py-2 px-4"
+          onChange={(event) => setTitle(event.target.value)}
+        />
       </div>
       <div className="flex flex-wrap p-6">
         {data.map((item) => {
           if (item.descrIns.toLowerCase().includes(title))
             return (
               <div className=" w-1/4" key={item.codW4}>
-                <div className="border w-full h-full p-6">{item.descrIns}
-                <div className="flex flex-wrap ">
-                {item.appelli.map((appello) => (
-                    <p className="bg-purple-200 m-1 px-1 rounded-lg">{appello.dataStr}</p>
-                ))}
-                </div>
+                <div className="border w-full h-full p-6">
+                  {item.descrIns}
+                  <div className="flex flex-wrap ">
+                    {item.appelli.map((appello) => (
+                      <p className="bg-purple-200 m-1 px-1 rounded-lg">
+                        {appello.dataStr}
+                      </p>
+                    ))}
+                  </div>
                 </div>
               </div>
             );
+           else
+            return <div></div>;
+
         })}
       </div>
     </div>
