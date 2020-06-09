42068df8daa4a6e7a44c7144556d74e763f6a6d9
Antongiacomo Polimeno <antongiacomo>
2020-06-09 18:25:40 +0200

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import "./tailwind.generated.css";
+//
 function App() {
+  const [data, setData] = useState([]);
+  const [title, setTitle] = useState('')
+  const config = {
+    headers: { "Access-Control-Allow-Origin": "*" },
+  };
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await axios(
+        "https://cors-anywhere.herokuapp.com/https://work.unimi.it/foProssimiEsami/json/F94",
+        config
+      );
+
+      setData(result.data);
+    };
+
+    fetchData();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="">
+      <div className="w-full h-10 px-4 my-6">
+      <input className="w-full h-full border py-2 px-4" onChange={event => setTitle(event.target.value)} />
+      </div>
+      <div className="flex flex-wrap p-6">
+        {data.map((item) => {
+          if (item.descrIns.toLowerCase().includes(title))
+            return (
+              <div className=" w-1/4" key={item.codW4}>
+                <div className="border w-full h-full p-6">{item.descrIns}
+                <div className="flex flex-wrap ">
+                {item.appelli.map((appello) => (
+                    <p className="bg-purple-200 m-1 px-1 rounded-lg">{appello.dataStr}</p>
+                ))}
+                </div>
+                </div>
+              </div>
+            );
+        })}
+      </div>
     </div>
   );
 }
