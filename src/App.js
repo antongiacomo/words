229e320f46f8c2663282807b4de2112bb7fb3ad7
Antongiacomo Polimeno <antongiacomo>
2020-06-10 09:57:52 +0200

    Search improved

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import "./tailwind.generated.css";
+import Block from "./Block";
+
 //
 function App() {
   const [data, setData] = useState([]);
-  const [title, setTitle] = useState("");
+  const [title, setTitle] = useState([""]);
+  //const [filter, setFilter] = useState("/");
 
   useEffect(() => {
     var config = {
@@ -23,35 +26,51 @@ function App() {
     fetchData();
   }, []);
 
+  function clear(item) {
+    var result = title.map((element) =>{
+
+    return (
+      item.descrIns.toLowerCase().includes(element) ||
+      item.appelli[0].docente.nome.toLowerCase().includes(element) ||
+      item.appelli[0].docente.cognome.toLowerCase().includes(element)
+    );
+    })
+
+    return result.some((item) => item);
+  }
+
   return (
     <div className="">
       <div className="w-full h-10 px-4 my-6">
+      {/* <input
+          className="w-full h-full border py-2 px-4 rounded-lg shadow"
+
+          onChange={(event) => setFilter("/" + event.target.value.toLowerCase())}
+        /> */}
         <input
-          className="w-full h-full border py-2 px-4"
-          onChange={(event) => setTitle(event.target.value)}
+          className="w-full  h-full border py-2 px-4 rounded-lg shadow"
+          onChange={(event) => setTitle(event.target.value.toLowerCase().split("&"))}
         />
       </div>
-      <div className="flex flex-wrap p-6">
-        {data.map((item) => {
-          if (item.descrIns.toLowerCase().includes(title))
+      <div className="flex flex-wrap p-3">
+        {data
+          .filter((item) => clear(item))
+          .map((item) => {
             return (
-              <div className=" w-1/4" key={item.codW4}>
-                <div className="border w-full h-full p-6">
-                  {item.descrIns}
-                  <div className="flex flex-wrap ">
-                    {item.appelli.map((appello) => (
-                      <p className="bg-purple-200 m-1 px-1 rounded-lg">
-                        {appello.dataStr}
-                      </p>
-                    ))}
-                  </div>
+              <div className="lg:w-1/4 md:w-1/3 sm:w-1/2 w-full  p-3" key={item.codW4}>
+
+                <div className="border w-full h-full p-4 rounded-lg shadow">
+                  <p className="font-medium text-lg mb-1">{item.descrIns}</p>
+                  <p className="text-sm mb-5">
+                    {item.appelli[0].docente.nome}{" "}
+                    {item.appelli[0].docente.cognome}
+                  </p>
+                <Block item={item ?? []} ></Block>
                 </div>
+
               </div>
             );
-           else
-            return <div></div>;
-
-        })}
+          })}
       </div>
     </div>
   );
