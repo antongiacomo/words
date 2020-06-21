8b3d0d28c4cc138aa9ce1eacb40da27233950e70
antongiacomo <antongiacomo@users.noreply.github.com>
2020-06-21 15:47:41 +0000

    deploy: 15e276f7896164a9a7b7cf77e2c68875fb006373

---
+++ precache-manifest.cabce22c042698c5fa4924138e7ef850.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c8468f9d8b1aa33e1f113b333b23d473",
+    "url": "/Words/index.html"
+  },
+  {
+    "revision": "a248598c3883b208e600",
+    "url": "/Words/static/css/2.70e35f80.chunk.css"
+  },
+  {
+    "revision": "c0c0d0108123b0868127",
+    "url": "/Words/static/css/main.3966a21d.chunk.css"
+  },
+  {
+    "revision": "a248598c3883b208e600",
+    "url": "/Words/static/js/2.047a5a73.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Words/static/js/2.047a5a73.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c0c0d0108123b0868127",
+    "url": "/Words/static/js/main.c542e1c1.chunk.js"
+  },
+  {
+    "revision": "0642f120b9aac9e2c4f4",
+    "url": "/Words/static/js/runtime-main.7d8c83a2.js"
+  }
+]);
